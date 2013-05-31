b7fe3b45ad56e3816f815bb277c115d21a66d0e0
zhangdongly <zhangdong147896325@163.com>
2013-05-31 10:11:08 +0800

    update

---
+++ Yixing/model/StatusForMethod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Yixing.model
+{
+    class StatusForMethod
+    {
+        Status status { set; get; }
+        List<String> texingList { set; get; }
+    }
+}
